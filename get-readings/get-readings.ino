59b4137f18874e1a617da3379d83d41fd46d2eea
Madhushanka <madhushankadds@Madhushankas-MacBook-Pro.local>
2023-04-25 10:27:47 +0530

    fix: fix simple bug in llatest merge

--- get-readings/get-readings.ino
+++ get-readings/get-readings.ino
@@ -20,7 +20,6 @@ struct Sensor {
   };
 
   void start(int MPU_6050_POWER_MODE_) {
-
     Wire.beginTransmission(MPU6050_ADDRESS);  // start the I2C communication (page 45)
     Wire.write(0x6B);                         // start the gyro power register in power mode (page 40)
     Wire.write(MPU_6050_POWER_MODE_);         // command to maintain the power mode (page 40)
@@ -28,16 +27,16 @@ struct Sensor {
   }
 };
 
-void TCA9548A_set_path(uint8_t bus)
-{
-  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
-  Wire.write(1 << bus);          // send byte to select bus
+void TCA9548A_set_path(uint8_t TCA9548A_ADDRESS_, uint8_t bus) {
+  Wire.beginTransmission(TCA9548A_ADDRESS_);   // TCA9548A address is 0x70
+  Wire.write(1 << bus);           // send byte to select bus
   Wire.endTransmission();
 }
 
 
 Sensor SENSOR1(0x68, 0x05, 0x10, 0x10, 0x1E, 0x14, 0x20, 0x00);
 Sensor SENSOR2(0x69, 0x05, 0x10, 0x10, 0x1E, 0x14, 0x20, 0x00);
+#define TCA9548A_ADDRESS 0x70
 
 char recievedChar;
 String output = "";
@@ -47,9 +46,9 @@ void setup() {
   Wire.setClock(400000);  // set I2C clock speed (page 5)
   Wire.begin();
   
-  TCA9548A_set_path(1);
+  TCA9548A_set_path(TCA9548A_ADDRESS, 1);
   SENSOR1.start(0x00);
-  TCA9548A_set_path(2);
+  TCA9548A_set_path(TCA9548A_ADDRESS, 2);
   SENSOR2.start(0x00);
 }
 
@@ -57,14 +56,14 @@ void loop() {
   if (Serial.available()) {
     recievedChar = Serial.read();
     if (recievedChar == '1') {
-      TCA9548A_set_path(1)
+      TCA9548A_set_path(TCA9548A_ADDRESS, 1);
       output = getFormatedOutput(output, SENSOR1.accelerometer.getReadings(), false);
       output = getFormatedOutput(output, SENSOR1.gyroscope.getReadings(), false);
       output = getFormatedOutput(output, SENSOR1.magnetometer.getReadings(), true);
       Serial.println(output);
 
     } else if (recievedChar == '2') {
-      TCA9548A_set_path)(2)
+      TCA9548A_set_path(TCA9548A_ADDRESS, 2);
       output = getFormatedOutput(output, SENSOR2.accelerometer.getReadings(), false);
       output = getFormatedOutput(output, SENSOR2.gyroscope.getReadings(), false);
       output = getFormatedOutput(output, SENSOR2.magnetometer.getReadings(), true);
